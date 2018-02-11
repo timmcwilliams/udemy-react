faebc5c65ed4cb30733a04705bd7676778f3f0b6
tmac4403 <drive911@gmail.com>
2018-02-11 00:17:36 -0600

    Onward

--- Indecision/React-indesion-unsolved/Public/scripts/app.js
+++ Indecision/React-indesion-unsolved/Public/scripts/app.js
@@ -1,7 +1,12 @@
 console.log("app is running");
 
 //JSX Java
-var template=<p>this is JSX from app.js!</p>;
+// var template=<p>this is JSX from app.js!</p>;
+var template = React.createElement(
+    "h1",
+    { id: "someid" },
+    "this is JSX from app.js!"
+  );
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
